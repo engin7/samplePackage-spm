fa4abc33fee790f5ff7e2dee4d6f771e96ed7078
Engin <kuk.engin@gmail.com>
2021-10-19 17:43:04 +0300

    public test

--- Package.swift
+++ Package.swift
@@ -8,8 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "samplePackage",
-            type: .dynamic, 
+            name: "samplePackage", 
             targets: ["samplePackage"]),
     ],
     dependencies: [
---
+++ Sources/Controllers/TestVC.swift
@@ -0,0 +1,13 @@
+//
+//  TestVC.swift
+//  
+//
+//  Created by Engin KUK on 19.10.2021.
+//
+
+import Foundation
+
+public class TestVC {
+    
+    public var test = ""
+}
