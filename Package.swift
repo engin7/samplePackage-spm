b3ee0b79e2e0ec71a7e022dafa4bd4464338e0ca
Engin <kuk.engin@gmail.com>
2021-10-19 18:03:57 +0300

    name change

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "samplePackage",
+    name: "PickFlavor",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "samplePackage", 
-            targets: ["samplePackage"]),
+            name: "PickFlavor",
+            targets: ["PickFlavor"]),
     ],
     dependencies: [
         .package(url: "https://github.com/jdg/MBProgressHUD", from: "1.1.0"),
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "samplePackage",
+            name: "PickFlavor",
             dependencies: []),
         .testTarget(
             name: "samplePackageTests",
-            dependencies: ["samplePackage"]),
+            dependencies: ["PickFlavor"]),
     ]
 )
